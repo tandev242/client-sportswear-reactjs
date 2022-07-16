342d71509bcfdcb77eaf6eb64e28530a201103ae
Tan Dev Zai <superjunior242@gmail.com>
2022-07-16 23:41:31 +0700

    add rating for product

--- src/components/layout/content/Content.js
+++ src/components/layout/content/Content.js
@@ -4,18 +4,17 @@ import HotProduct from "./products/HotProduct";
 import RecommendedProduct from "./products/RecommendedProduct";
 import BrandBanner from "./brand/BrandBanner";
 import PositionBanner from "./position/PositionBanner";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Content = ({ brand, category }) => {
   const { recommendByBehaviorList } = useSelector((state) => state.recommend)
-  console.log(recommendByBehaviorList)
   return (
     <div className="content">
       <div className="container">
         {recommendByBehaviorList.length > 0 ?
-          <RecommendedProduct products={recommendByBehaviorList} /> :
-          <HotProduct />
+          <RecommendedProduct products={recommendByBehaviorList} /> : null
         }
+        <HotProduct />
         <CategoryBanner category={category} />
         <BrandBanner brand={brand} />
         <PositionBanner />
--- src/views/ProductDetails.js
+++ src/views/ProductDetails.js
@@ -238,6 +238,22 @@ const ProductDetails = () => {
   };
   // Show confirm alert to redirect
 
+  const getStarStyle = (product) => {
+    let ratingTotal = 0;
+    let ratingAdv = 0;
+    if(product.reviews.length > 0){
+      for(let item of product.reviews){
+        ratingTotal += item.rating ;
+      }
+      ratingAdv = ratingTotal/product.reviews.length;
+    }
+    let percentClip = 100 - ratingAdv * 20;
+    let starStyle = {
+      clipPath: `inset(0 ${percentClip}% 0 0)`,
+    }
+    return starStyle;
+  }
+
   if (Object.keys(product).length === 0) {
     return null;
   }
@@ -280,7 +296,7 @@ const ProductDetails = () => {
                     <div className="photos">
                       <Slider
                         asNavFor={slidePhotos}
-                        ref={(slide) => setSlideSub(slide)}
+ref={(slide) => setSlideSub(slide)}
                         {...photoSettings}
                       >
                         {product.productPictures?.map((image) => (
@@ -297,6 +313,71 @@ const ProductDetails = () => {
                 <div className="body-heading">
                   <h3>{product?.name}</h3>
                 </div>
+                <div className="rating-result">
+                  <div className="rating">
+                    <div className="star">
+                        <label>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                          </svg>
+                        </label>
+                        <label>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                          </svg>
+                        </label>
+                        <label>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                          </svg>
+                        </label>
+                        <label>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                          </svg>
+                        </label>
+                        <label>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                          </svg>
+                        </label>
+                        <div className="star-1" style = {getStarStyle(product)}>
+                          <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                          </label>
+                          <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                          </label>
+                          <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                          </label>
+                          <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                          </label>
+                          <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="seperate">
+
+                    </div>
+                    <div className="review-count">
+                      {product.reviews.length ? `${product.reviews.length} đánh giá` : 'Chưa có đánh giá cho sản phẩm này'}
+                    </div>
+                </div>
+                
                 <div className="body-price">
                   <span className="body-price__old">
                     ₫{new Intl.NumberFormat("de-DE").format(product.price)}
@@ -309,7 +390,7 @@ const ProductDetails = () => {
                     )}
                   </span>
                 </div>
-                <div className="body-brand">
+<div className="body-brand">
                   <span className="body-brand__label">Thương hiệu: </span>
                   <p className="body-brand__name">{product.brand?.name}</p>
                 </div>
@@ -322,7 +403,7 @@ const ProductDetails = () => {
                     {product.sizes?.map((size) => (
                       <label
                         htmlFor={size.size._id}
-                        className="body-size__options-item"
+                        className=  "body-size__options-item"
                       >
                         <input
                           type="radio"
@@ -331,9 +412,10 @@ const ProductDetails = () => {
                           id={size.size._id}
                           value={size.size._id}
                           required
+                          disabled = {size.quantity === 0 ? true : false}
                           onChange={() => handleChangeSize(size.size)}
                         />
-                        <div className="body-size__options-item__label">
+                        <div className={ size.quantity === 0 ? "body-size__options-item__label disable-option" : "body-size__options-item__label"}>
                           {size.size.size}
                           <div className="body-size__options-item__label-note">
                             <p>Gợi ý ({size.size.description})</p>
@@ -373,7 +455,7 @@ const ProductDetails = () => {
                     className="btn body-btn__buy"
                     onClick={() => handlePayNow()}
                   >
-                    Mua ngay
+Mua ngay
                   </span>
                   <span
                     type="submit"
